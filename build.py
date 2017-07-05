421b487f005ea4f702c143b9614e1613a1d50173
Shreyas Balakrishna <shreyas.balakrishna@fphcare.co.nz>
2017-07-06 10:06:52 +1200

    Uploading package to remote as part of build

--- build.py
+++ build.py
@@ -13,9 +13,9 @@ try:
     os.system("conan package .. --build_folder=../")
     print(os.listdir("."))
     print(os.listdir("./lib"))
-    os.system("cd .. && conan export user/channel") # This will improve next 0.25 with an --cwd=.. arg
-    os.system("conan package_files Test/0.1@user/channel -f")  # -f in case it exists, overwrite
-    os.system("conan search Test/0.1@user/channel") # To show the package binary
-    # os.system("conan upload Test/0.1@user/channel --all -r=")
+    os.system("cd .. && conan export myuser/testing") # This will improve next 0.25 with an --cwd=.. arg
+    os.system("conan package_files Test/0.1@myuser/testing -f")  # -f in case it exists, overwrite
+    os.system("conan search Test/0.1@myuser/testing") # To show the package binary
+    os.system("conan upload Test/0.1@myuser/testing --all -r=artifactory")
 finally:
     os.chdir(old)
